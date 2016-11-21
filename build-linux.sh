10c40485b98b5e35da0c4f45be447cf57406bc8f
Lukáš Lalinský <lukas@oxygene.sk>
2016-11-22 00:51:52 +0100

    Build script

---
+++ build-linux.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd $(dirname $0)
+BASE_DIR=$(pwd)
+
+source common.sh
+
+if [ ! -e $FFMPEG_TARBALL ]
+then
+	wget $FFMPEG_TARBALL_URL
+fi
+
+apt-get install -y yasm pkg-config gcc-multilib
+
+: ${ARCH?}
+
+TARGET=$BASE_DIR/ffmpeg-$FFMPEG_VERSION-audio-linux-$ARCH
+FFMPEG_CONFIGURE_FLAGS+=(--prefix=$TARGET)
+
+case $ARCH in
+i686)
+    FFMPEG_CONFIGURE_FLAGS+=(--cc="gcc -m32")
+    ;;
+x86_64)
+    ;;
+*)
+    echo "Unknown architecture"
+    exit 1
+esac
+
+BUILD_DIR=$(mktemp -d -p $(pwd) build.XXXXXXXX)
+trap 'rm -rf $BUILD_DIR' EXIT
+
+cd $BUILD_DIR
+tar --strip-components=1 -xf $BASE_DIR/$FFMPEG_TARBALL
+
+./configure "${FFMPEG_CONFIGURE_FLAGS[@]}"
+make -j 4
+make install
+
+chown -r $(uid $0):$(gid $0) -R $TARGET
